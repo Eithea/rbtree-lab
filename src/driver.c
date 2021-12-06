f89cc928ac87957208ffd70290a0dbfe5b920109
Eithea <oguraff94@gmail.com>
2021-12-07 01:54:13 +0900

    aa

--- src/driver.c
+++ src/driver.c
@@ -1,5 +1,21 @@
 #include "rbtree.h"
 
-int main(int argc, char *argv[]) {
-
+void main()
+{
+    rbtree *t = new_rbtree();
+    rbtree_insert(t, 2);
+    rbtree_insert(t, 1);
+    rbtree_insert(t, 8);
+    rbtree_insert(t, 9);
+    rbtree_insert(t, 7);
+    rbtree_insert(t, 6);
+    rbtree_insert(t, 4);
+    rbtree_insert(t, 5);
+    node_t *f = rbtree_find(t, 4);
+    rbtree_erase(t, f);
+    f = rbtree_find(t, 1);
+    rbtree_erase(t, f);
+    f = rbtree_find(t, 8);
+    rbtree_erase(t, f);
+    testprint(t, t->root);
 }
